d653debf2a4e79ade53b9548c3ea6d49a6d3db09
Sergey Mishin <smishin91@yandex.ru>
2018-05-28 22:42:49 +0300

    Create SportsStore Web app

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            repository = productRepository; 
+        }
+
+        public ViewResult List() => View(repository.Products);
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
