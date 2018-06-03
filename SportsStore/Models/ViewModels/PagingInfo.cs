47025291b117a3dd87bf5819c0916409120c3171
Sergey Mishin <smishin91@yandex.ru>
2018-06-03 23:06:15 +0300

    add EF + bootstrap

---
+++ SportsStore/Models/ViewModels/PagingInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SportsStore.Models.ViewModels
+{
+    public class PagingInfo
+    {
+        public int TotalItems { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+
+        //Ceiling - Возвращает наименьшее целое число, 
+        //которое больше или равно заданному числу с плавающей 
+        //запятой двойной точности. (Floor - наоборот)
+        public int TotalPages =>
+        (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage);
+    }
+}
